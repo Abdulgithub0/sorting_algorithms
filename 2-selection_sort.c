5df6ad13bd1c88776a0386a30b64516112a7e702
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-25 14:41:50 +0100

    implemented selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,22 +11,27 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, temp, k;
 
+	bool flag;
+
 	if (size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		k = i;
+		flag = false;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[k] > array[j])
 			{
 				k = j;
+				flag = true;
 			}
 		}
+		if (!flag)
+			return;
 		temp = array[k];
 		array[k] = array[i];
 		array[i] = temp;
 		print_array(array, size);
 	}
-		
 }
